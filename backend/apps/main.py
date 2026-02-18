dfc92de0fb6e6355010584aa33d959f901609b69
Prateek Srivastava <110523559+prateek-workspace@users.noreply.github.com>
2026-02-18 15:16:47 +0530

    Update main.py

--- backend/apps/main.py
+++ backend/apps/main.py
@@ -23,7 +23,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
     ],
-    allow_origin_regex=r"https://(.*\.)?skstudentpath\.com|https://sk-mvp.*\.pages\.dev",
+    allow_origin_regex=r"https://(.*\.)?skstudentpath\.com|https://.*\.sk-mvp\.pages\.dev",
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["*"],
